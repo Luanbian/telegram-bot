3cb58442d8840c9dedb5ec05f521388850a1eab1
Luan Almeida <luan.almeida@jbtec.com.br>
2025-04-24 23:02:43 -0300

    fix: constants concat url format

--- src/constants/ngrok.ts
+++ src/constants/ngrok.ts
@@ -1,1 +1,1 @@
-export const NGROK_URL = process.env.NGROK_URL || '';
+export const NGROK_URL = process.env.NGROK_URL || 'http://localhost:3000';
--- src/constants/telegram.ts
+++ src/constants/telegram.ts
@@ -4,5 +4,5 @@ export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || '';
 export const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
 
 export const WEBHOOK_ENDPOINT = `/webhook/${TELEGRAM_TOKEN}`;
-export const WEBHOOK_URL = `${NGROK_URL}/${WEBHOOK_ENDPOINT}`;
+export const WEBHOOK_URL = NGROK_URL + WEBHOOK_ENDPOINT;
 export const SET_TELEGRAM_WEBHOOK_URL = `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`;
